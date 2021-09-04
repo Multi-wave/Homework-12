100c01b8b439b68ceade6931f49a5bec3051b6a2
Multi-wave <87474783+Multi-wave@users.noreply.github.com>
2021-09-04 11:12:10 +0400

    Update Homework 12.2.cpp

--- Homework 12.2.cpp
+++ Homework 12.2.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
 int main() {
-    int numbers[] = {114,111,106,107,108,105,115,108,110,109,112,113,116,117,118};
-
-    for (int i = 0 ; i < 15 ; ++i){
-        for (int j = i+1 ; j < 15 ; ++j ) if (numbers[j] == numbers[i]) return numbers[i];
+    int numbers[] = { 114,111,106,107,108,105,115,108,110,109,112,113,116,117,118 };
+    int sumMas = numbers[0];
+    int max = numbers[0];
+    for (int i = 1; i < 15; ++i) {
+        if (numbers[i] > max) max = numbers[i];
+        sumMas += numbers[i];
     }
-    std::cout << "There are no matches!";
+    int sumExp = 0;
+    ++max;
+    for (int i = 0; i < 15; ++i) {
+        sumExp += max - i;
+    }
+    std::cout << "Match = " << max - (sumExp - sumMas);
 }
